973ab02b64ad88296cd014cc1d570aa07bf7626b
yerakulmanov <200103127@stu.sdu.edu.kz>
2023-06-11 17:39:43 +0600

    Add a box layout

--- app/src/main/java/com/example/birthdaycard/MainActivity.kt
+++ app/src/main/java/com/example/birthdaycard/MainActivity.kt
@@ -5,8 +5,8 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
@@ -29,7 +29,6 @@ class MainActivity : ComponentActivity() {
             BirthdayCardTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
                     GreetingText(message = "Happy Birthday Sam!", from = "From Emma")
@@ -63,9 +62,17 @@ class MainActivity : ComponentActivity() {
     }
 
     @Composable
-    fun GreetingImage(message: String, from:String, modifier: Modifier = Modifier){
+    fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) {
         val image = painterResource(R.drawable.androidparty)
-        Image(painter = image, contentDescription = null)
+        Box {
+            Image(painter = image, contentDescription = null)
+            GreetingText(
+                message = message, from = from,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(8.dp)
+            )
+        }
     }
 
 
