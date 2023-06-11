1161de069381871d72619edc280e7af6a84b8d14
yerakulmanov <200103127@stu.sdu.edu.kz>
2023-06-11 18:31:55 +0600

    Update alignment

--- app/src/main/java/com/example/birthdaycard/MainActivity.kt
+++ app/src/main/java/com/example/birthdaycard/MainActivity.kt
@@ -59,7 +59,7 @@ class MainActivity : ComponentActivity() {
                 fontSize = 36.sp,
                 modifier = Modifier
                     .padding(16.dp)
-                    .align(alignment = Alignment.End)
+                    .align(alignment = Alignment.CenterHorizontally)
             )
         }
     }
